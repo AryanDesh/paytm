8cc32f62ab2c9c5551e6be0da07d97489c402b3a
Aryan Deshmukh <aryan.deshmukh21@vit.edu>
2024-07-15 12:49:57 +0530

    userbackend

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,14 @@
 const express = require("express");
+const cors = require('cors');
+const app = express();
+const rootRouter = require("./routes/index");
 
+app.use(cors());
+app.use(express.json());
+app.use("/api/v1" , rootRouter);
 
+const { User, Account } = require('./db')
+
+app.listen(3000, () => {
+  console.log(`Server is running on port 3000`);
+});
